e817b8dcbfd5db78dfdba6cec52ada3a3c8984b8
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-12-28 20:08:33 -0400

    [UPDATE] Cleaning index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import SimpleButton from "../components/buttons/SimpleButton";
-
 export default function Home(): JSX.Element {
-  return (
-    <SimpleButton buttonType="PRIMARY" onClick={() => console.log("Hola")}>
-      Adios
-    </SimpleButton>
-  );
+  return <p>hello world!</p>;
 }
