b500d326a4afa6cc5c47de2c59a653f15d2c87b8
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-01-03 16:29:54 -0400

    [ADD] Component Post

---
+++ src/components/items/Post.tsx
@@ -0,0 +1,58 @@
+import { AiFillHeart } from "react-icons/ai";
+import { BiCommentDetail } from "react-icons/bi";
+import { HiShare } from "react-icons/hi";
+
+import styles from "../../styles/items/Post.module.scss";
+
+type Props = {
+  profile: string;
+  title: string;
+  description: string;
+  likes: number;
+  comments: number;
+  shares: number;
+};
+
+const Post: React.FC<Props> = ({
+  profile,
+  title,
+  description,
+  likes,
+  comments,
+  shares,
+}: Props): JSX.Element => {
+  return (
+    <article className={styles.post}>
+      <picture className={styles.profilePicture}>
+        <img src={profile} alt="User" />
+      </picture>
+      <div className={styles.postText}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+      <hr />
+      <ul className={styles.social}>
+        <li>
+          <button className={styles.socialButton}>
+            <AiFillHeart className={styles.socialSVG} />
+            <p className={styles.socialText}>{likes}</p>
+          </button>
+        </li>
+        <li>
+          <button className={styles.socialButton}>
+            <BiCommentDetail className={styles.socialSVG} />
+            <p className={styles.socialText}>{comments}</p>
+          </button>
+        </li>
+        <li>
+          <button className={styles.socialButton}>
+            <HiShare className={styles.socialSVG} />
+            <p className={styles.socialText}>{shares}</p>
+          </button>
+        </li>
+      </ul>
+    </article>
+  );
+};
+
+export default Post;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import Card from "../components/items/Card";
+import Post from "../components/items/Post";
 
 export default function Home(): JSX.Element {
+  const description =
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, illum? Deleniti culpa commodi vitae error facilis aperiam animi, fugit harum.";
+  const profile = "https://thispersondoesnotexist.com/image";
   return (
-    <p> Hola mundo</p>
+    <Card
+      backgroundImageUrl={
+        "https://cdn.pixabay.com/photo/2017/08/03/12/44/nature-2576047_960_720.jpg"
+      }
+      isBluredBackground
+      isDefaultSize>
+      <Post
+        profile={profile}
+        title="Titulo de texto con texto"
+        description={description}
+        likes={2}
+        comments={5}
+        shares={3}
+      />
+    </Card>
   );
-};
+}
