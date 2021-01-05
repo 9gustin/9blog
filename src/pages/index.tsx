3e85e9a1ce5909f0c632e1aa5f2a91cdd59ec3b1
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-05 15:51:36 -0300

    Add httpClient for 9blog backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
+import { useEffect } from "react";
+
+import { CATEGORIES } from "../services/9blog/config";
+import HttpClient from "../services/9blog/httpClient";
+
 export default function Home(): JSX.Element {
   return <p>Hola mundo</p>;
 }
---
+++ src/services/9blog/config.ts
@@ -0,0 +1,11 @@
+export const API_URL = "https://backend-9blog.herokuapp.com/";
+
+export const POSTS = "/posts";
+
+export const PROFILES = "/profiles";
+
+export const ACTIONS = "/actions";
+
+export const ACTIONTYPES = "/action-types";
+
+export const CATEGORIES = "/categories";
